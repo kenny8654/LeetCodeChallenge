3765b14c229877671a1a827083d211881c1b9621
kenny8654 <37100988+kenny8654@users.noreply.github.com>
2022-09-21 13:10:55 +0800

    Create 15_threeSum.cpp

---
+++ 15_threeSum.cpp
@@ -0,0 +1,31 @@
+/**
+Runtime: 132 ms, faster than 65.28% of C++ online submissions for 3Sum.
+Memory Usage: 19.9 MB, less than 63.33% of C++ online submissions for 3Sum.
+**/
+class Solution {
+public:
+  vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> ret;
+    std::sort(nums.begin(), nums.end());
+    for(int i=0; i<nums.size()-2; i++){
+      if(nums[i] > 0) break;
+      if(i>0 && (nums[i]==nums[i-1])) continue;  
+      int j=i+1, k=nums.size()-1;
+
+      while(j < k){
+        int sum = nums[j]+nums[k]+nums[i];
+        if(sum == 0){
+          ret.push_back({nums[i], nums[j], nums[k]});
+          while(j+1<k && nums[j]==nums[j+1]) j++;
+          while(j<k-1 && nums[k]==nums[k-1]) k--;
+          j++; k--;
+        }
+        else if(sum < 0)
+          j++;
+        else
+          k--;
+      }
+    }
+    return ret;
+  }
+};
