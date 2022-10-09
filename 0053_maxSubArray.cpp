457e2392fd6809e3c73872f1092f3c127c08d9b2
kenny8654 <37100988+kenny8654@users.noreply.github.com>
2022-10-09 21:48:46 +0800

    Update 0053_maxSubArray.cpp

--- 0053_maxSubArray.cpp
+++ 0053_maxSubArray.cpp
@@ -2,12 +2,12 @@
 Runtime: 113 ms, faster than 96.65% of C++ online submissions for Maximum Subarray.
 Memory Usage: 67.7 MB, less than 90.32% of C++ online submissions for Maximum Subarray.
 **/
+#define INTMIN -1000000
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int ret = nums[0], curr=nums[0];
-        if(nums.size()==1) return ret;
-        for(int i=1; i< nums.size(); i++){
+        int ret=INTMIN, curr=INTMIN;
+        for(int i=0; i< nums.size(); i++){
             curr = max(nums[i], curr+nums[i]);
             ret = max(ret, curr);
         }
