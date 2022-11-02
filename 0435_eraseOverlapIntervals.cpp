cf9b6afb1af69ce0c563814417817c0591358cf5
kenny8654 <37100988+kenny8654@users.noreply.github.com>
2022-11-02 12:51:45 +0800

    Update 0435_eraseOverlapIntervals.cpp

--- 0435_eraseOverlapIntervals.cpp
+++ 0435_eraseOverlapIntervals.cpp
@@ -1,22 +1,25 @@
 /**
-Runtime: 612 ms, faster than 77.98% of C++ online submissions for Non-overlapping Intervals.
-Memory Usage: 89.9 MB, less than 20.17% of C++ online submissions for Non-overlapping Intervals.
+Runtime: 303 ms, faster than 100.00% of C++ online submissions for Non-overlapping Intervals.
+Memory Usage: 90.7 MB, less than 17.68% of C++ online submissions for Non-overlapping Intervals.
 **/
 class Solution {
 public:
-    static bool comp(vector<int>& a, vector<int>& b){
-        return a[1] < b[1];
-    }
-    
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         int ret = 0, num=INT_MIN;
-        sort(intervals.begin(), intervals.end(), comp);
-        for(auto& vec:intervals){
-            if(vec[0] >= num)
-                num = vec[1];
+        int n = intervals.size();
+        
+        pair<int,int> p[n];
+        for(int i=0;i<n;i++){
+            p[i].first = intervals[i][0];
+            p[i].second = intervals[i][1];
+        }
+        
+        sort(p, p+n, [](pair<int,int> &vp1, pair<int,int> &vp2){return vp1.second<vp2.second;});
+        for(auto& [num1, num2]:p){
+            if(num1 >= num)
+                num = num2;
             else
                 ret++;
-            
         }
         return ret;
     }
