4760990898d9e7f2b67cc2302cf9c29fb94d9326
kenny8654 <37100988+kenny8654@users.noreply.github.com>
2022-11-15 22:48:38 +0800

    Create 0128_longestConsecutive.cpp

---
+++ 0128_longestConsecutive.cpp
@@ -0,0 +1,29 @@
+/**
+Runtime: 181 ms, Beats: 81.31%
+Memory: 52.1 MB, Beats: 11.1%
+**/
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int ret = 0;
+        unordered_map<int, int> map;
+
+        for(auto i:nums){
+            if(map[i] != 0) continue;
+            if(map[i-1] == 0 && map[i+1] == 0){
+                map[i] = 1;
+            }else if(map[i-1] != 0 && map[i+1] != 0){
+                map[i] = map[i-1] + map[i+1] + 1;
+                map[i-map[i-1]] = map[i+map[i+1]] = map[i];
+            }else if(map[i-1] != 0){
+                map[i] = map[i-1] + 1;
+                map[i-map[i-1]] = map[i];
+            }else{
+                map[i] = map[i+1] + 1;
+                map[i+map[i+1]] = map[i];
+            }
+            ret = max(ret, map[i]);
+        }
+        return ret;
+    }
+};
