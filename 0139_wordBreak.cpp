1cd6e3f98a2b91f30fd32d37f7333e598a7ea8f6
kenny8654 <37100988+kenny8654@users.noreply.github.com>
2022-11-16 13:59:53 +0800

    Create 0139_wordBreak.cpp

---
+++ 0139_wordBreak.cpp
@@ -0,0 +1,20 @@
+/**
+Runtime: 4 ms, Beats: 92.62%
+Memory: 7.6 MB, Beats: 88.15%
+**/
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<bool> dp(s.size()+1, false);
+        dp[0] = true;
+        cout<<s<<endl;
+        for(int i=1; i<dp.size(); i++){
+            for(auto& word:wordDict){
+                if(dp[i-1] == true && s.substr(i-1, word.size()) == word){
+                    dp[i+word.size()-1] = dp[i-1];
+                }
+            }
+        }
+        return dp[dp.size()-1];
+    }
+};
