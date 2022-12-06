ce731ce0572ef483f4dad1efc6e473a7d49e95f5
kenny8654 <37100988+kenny8654@users.noreply.github.com>
2022-12-06 13:42:05 +0800

    Update 0062_uniquePaths.cpp

--- 0062_uniquePaths.cpp
+++ 0062_uniquePaths.cpp
@@ -1,7 +1,27 @@
 /**
 Runtime: 0 ms, Beats: 100%
+Memory: 6.6 MB, Beats: 29.66%
+**/
+//DP
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> dp(m, vector<int>(n, 1));
+
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+    }
+    return dp[m-1][n-1];
+};
+
+/**
+Runtime: 0 ms, Beats: 100%
 Memory: 6 MB, Beats: 79.61%
 **/
+// Math
 class Solution {
 public:
     int uniquePaths(int m, int n) {
